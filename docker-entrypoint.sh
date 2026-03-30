03321f279e82aac464c9007e21815891d49540c0
Aneta Drahonovska <zuni@email.cz>
2026-03-30 18:41:09 +0200

    Rewrote the docker file so that it derives from pandoc/typst docker image

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,12 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 set -e
 
 # Add a low-privileged user to run the application
 USER_ID=$(stat -c '%u' /projects 2>/dev/null || echo 1000)
-useradd -m -u "$USER_ID" --non-unique sereto
+if [ "$USER_ID" = "0" ]; then
+    USER_ID=1000
+fi
+adduser -D -h /home/sereto -u "$USER_ID" sereto
 
 # Change permissions
 chown -R sereto:sereto /home/sereto/
@@ -11,19 +14,19 @@ chown -R sereto:sereto /home/sereto/
 # Add sereto user to the group of /projects if it exists
 if [ -d /projects ]; then
     PROJECTS_GID=$(stat -c '%g' /projects)
-    if ! getent group "$PROJECTS_GID" >/dev/null; then
-        groupadd -g "$PROJECTS_GID" projectsgrp
+    if ! getent group "$PROJECTS_GID" >/dev/null 2>&1; then
+        addgroup -g "$PROJECTS_GID" projectsgrp
     fi
-    usermod -aG "$PROJECTS_GID" sereto
+    addgroup sereto "$(getent group "$PROJECTS_GID" | cut -d: -f1)"
 fi
 
 # Add sereto user to the group of /templates if it exists
 if [ -d /templates ]; then
     TEMPLATES_GID=$(stat -c '%g' /templates)
-    if ! getent group "$TEMPLATES_GID" >/dev/null; then
-        groupadd -g "$TEMPLATES_GID" templatesgrp
+    if ! getent group "$TEMPLATES_GID" >/dev/null 2>&1; then
+        addgroup -g "$TEMPLATES_GID" templatesgrp
     fi
-    usermod -aG "$TEMPLATES_GID" sereto
+    addgroup sereto "$(getent group "$TEMPLATES_GID" | cut -d: -f1)"
 fi
 
 # Install requirements from /templates/requirements.txt if it exists
