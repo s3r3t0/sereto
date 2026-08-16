894c6bd880bbf2ed57198307a7c1b0b6e0eab35e
Jan Havránek <13387356+h4vry@users.noreply.github.com>
2026-08-16 23:48:30 +0200

    Add __reduce__ to SeretoDate

--- sereto/models/date.py
+++ sereto/models/date.py
@@ -64,13 +64,12 @@ class SeretoDate(date):
     def __repr__(self) -> str:
         return f"SeretoDate('{self!s}')"
 
+    def __reduce__(self) -> tuple[type[SeretoDate], tuple[int, int, int]]:
+        return (self.__class__, (self.year, self.month, self.day))
+    
     def __deepcopy__(self, memo: dict[int, Any]) -> SeretoDate:
         """Support deepcopy by creating a new instance with the same date components."""
         return SeretoDate(self.year, self.month, self.day)
-
-    def __reduce_ex__(self, protocol: SupportsIndex) -> tuple[type[SeretoDate], tuple[int, int, int]]:
-        """Support pickling by returning constructor args."""
-        return (self.__class__, (self.year, self.month, self.day))
 
     @classmethod
     def __get_pydantic_core_schema__(
